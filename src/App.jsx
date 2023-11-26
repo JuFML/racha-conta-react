ce7f3339e5ef39a57abde54c6ffd5346e6bd1f0f
Juliana Fernandez <julianafernandezlima@hotmail.com>
2023-11-26 13:39:51 -0300

    Adiciona markup amigos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,93 @@
+import { useState } from "react";
+
 const App = () => {
-  return <h1>Boilerplate Vite + React</h1>;
+  const initialFriends = [
+    {
+      Id: crypto.randomUUID(),
+      friendsName: "Henrique",
+      photo: "henrique-48.jpg",
+      balance: 0,
+      selected: false,
+    },
+    {
+      Id: crypto.randomUUID(),
+      friendsName: "Renata",
+      photo: "renata-48.jpg",
+      balance: -20,
+      selected: false,
+    },
+    {
+      Id: crypto.randomUUID(),
+      friendsName: "Antonio",
+      photo: "antonio-48.jpg",
+      balance: 7,
+      selected: false,
+    },
+  ];
+
+  const [friends, setFriends] = useState(initialFriends);
+  const [showForm, setShowForm] = useState(false);
+
+  let allFriends = [...friends];
+
+  const getMessageInfo = (balance) =>
+    balance > 0
+      ? {
+          message: `Você deve ${balance} reais`,
+          color: "green-credit",
+        }
+      : balance < 0
+      ? {
+          message: `Te deve ${-balance} reais`,
+          color: "red-debit",
+        }
+      : {
+          message: `Estão quites`,
+          color: "white-neutral",
+        };
+
+  const handleSelectClick = (name) => {
+    allFriends = [...friends];
+    console.log(allFriends);
+    allFriends.filter(
+      (friend) => friend.friendsName === name
+    )[0].selected = true;
+    // console.log((clickedFriend[0].selected = true));
+    console.log(allFriends);
+    // setFriends((prev) => [...prev, ...(clickedFriend.selected === true)]);
+    setShowForm(true);
+  };
+
+  return (
+    <>
+      <header className="header">HEADER</header>
+      <main className="app">
+        <aside className="sidebar">
+          <ul>
+            {allFriends.map(({ friendsName, photo, balance }, index) => {
+              const { message, color } = getMessageInfo(balance);
+
+              return (
+                <li key={index}>
+                  <img src={photo} alt={`avatar ${friendsName}`} />
+                  <h3>{friendsName}</h3>
+                  <p className={color}>{message}</p>
+                  <button
+                    className="button"
+                    onClick={() => handleSelectClick(friendsName)}
+                  >
+                    Selecionar
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+          <button className="button">Adicionar Amigo(a)</button>
+        </aside>
+        {showForm && <>Mostra FORM</>}
+      </main>
+    </>
+  );
 };
 
 export default App;
