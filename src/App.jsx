4e3b6758089a70b972a7b9d8534120aa2f189e67
JuFML <julianafernandezlima@hotmail.com>
2024-05-20 17:21:53 -0300

    Refatoração: useSttae -> useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,116 +1,91 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialFriends = [
+  {id: crypto.randomUUID(), name: "Henrique", photo: "henrique-48.jpg", balance: 0, selected: false},
+  {id: crypto.randomUUID(), name: "Renata", photo: "renata-48.jpg", balance: -20, selected: false},
+  {id: crypto.randomUUID(), name: "Antonio", photo: "antonio-48.jpg", balance: 7, selected: false},
+];
+
+const reducer = (state, action) => ({
+  set_friends: {...state, friends: action.friends},
+  set_selectedFriend: {...state, selectedFriend: action.selectedFriend},
+  set_totalBill: {...state, totalBill: action.totalBill},
+  set_costs: {...state, costs: action.costs},
+  set_whoIsGonnaPay: {...state, whoIsGonnaPay: action.whoIsGonnaPay},
+  set_showFormAddFriend: {...state, showFormAddFriend: action.showFormAddFriend},
+  set_friendName: {...state, friendName: action.friendName},
+  set_friendPhoto: {...state, friendPhoto: action.friendPhoto},
+})[action.type] || state
 
 const App = () => {
-  const initialFriends = [
-    {
-      id: crypto.randomUUID(),
-      name: "Henrique",
-      photo: "henrique-48.jpg",
-      balance: 0,
-      selected: false,
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Renata",
-      photo: "renata-48.jpg",
-      balance: -20,
-      selected: false,
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Antonio",
-      photo: "antonio-48.jpg",
-      balance: 7,
-      selected: false,
-    },
-  ];
+  const [state, dispatch] = useReducer(reducer, {friends: initialFriends, selectedFriend: null, totalBill: "", costs: "", whoIsGonnaPay: "you", 
+  showFormAddFriend: false, friendName: "", friendPhoto: ""})
 
-  const [friends, setFriends] = useState(initialFriends);
-  const [selectedFriend, setSelectedFriend] = useState(null);
-  const [totalBill, setTotalBill] = useState("");
-  const [costs, setCosts] = useState("");
-  const [whoIsGonnaPay, setWhoIsGonnaPay] = useState("you");
-  const [showFormAddFriend, setShowFormAddFriend] = useState(false)
-  const [friendName, setFriendName] = useState("");
-  const [friendPhoto, setFriendPhoto] = useState("");
-
-  const getMessageInfo = (balance) =>
-    balance > 0
-      ? {
-          message: `Você deve ${balance} reais`,
-          color: "green-credit",
-        }
-      : balance < 0
-      ? {
-          message: `Te deve ${-balance} reais`,
-          color: "red-debit",
-        }
-      : {
-          message: `Estão quites`,
-          color: "white-neutral",
-        };
+  const getMessageInfo = (balance) => {
+    let messageInfo;
+  
+    if (balance > 0) {
+      messageInfo = {
+        message: `Você deve ${balance} reais`,
+        color: "green-credit",
+      };
+    } else if (balance < 0) {
+      messageInfo = {
+        message: `Te deve ${-balance} reais`,
+        color: "red-debit",
+      };
+    } else {
+      messageInfo = {
+        message: `Estão quites`,
+        color: "white-neutral",
+      };
+    }
+  
+    return messageInfo;
+  };
 
   const handleClickFriend = (friend) => {
-    setSelectedFriend((prev) => (prev?.id === friend.id ? null : friend));
+    dispatch({type: "set_selectedFriend", selectedFriend: state.selectedFriend?.id === friend.id ? null : friend})
   };
 
   const handleClickCalculateBill = (e) => {
     e.preventDefault();
-    const balance = +totalBill - +costs;
+    const balance = +state.totalBill - +state.costs;
 
-    setFriends((prev) =>
-      prev.map((friend) =>
-        friend.id === selectedFriend.id
-          ? {
-              ...friend,
-              balance:
-                whoIsGonnaPay === "you"
-                  ? selectedFriend.balance - balance
-                  : selectedFriend.balance + balance,
-            }
-          : friend
-      )
-    );
-    setTotalBill("");
-    setCosts("");
-    setSelectedFriend(null);
+    dispatch({type: "set_friends", friends: state.friends?.map((friend) =>
+    friend.id === state.selectedFriend.id
+      ? {
+          ...friend,
+          balance:
+            state.whoIsGonnaPay === "you"
+              ? state.selectedFriend.balance - balance
+              : state.selectedFriend.balance + balance,
+        }
+      : friend
+  )})
+
+    dispatch({type: "set_totalBill", totalBill: ""})
+    dispatch({type: "set_costs", costs: ""})
+    dispatch({type: "set_selectedFriend", selectedFriend: null})
   };
 
-  const handleChangeGetTotalBill = (e) => {
-    setTotalBill(e.target.value);
-  };
+  const handleChangeGetTotalBill = (e) => dispatch({type: "set_totalBill", totalBill: e.target.value})
 
-  const handleChangeGetCosts = (e) => {
-    setCosts(e.target.value);
-  };
+  const handleChangeGetCosts = (e) => dispatch({type: "set_costs", costs: e.target.value})
 
-  const handleSelectWhoWillPay = (e) => {
-    setWhoIsGonnaPay(e.target.value);
-  };
+  const handleSelectWhoWillPay = (e) => dispatch({type: "set_whoIsGonnaPay", whoIsGonnaPay: e.target.value})
 
-  const handleClickShowAddFriend = () => {
-    setShowFormAddFriend(!showFormAddFriend)
-  }
+  const handleClickShowAddFriend = () => dispatch({type: "set_showFormAddFriend", showFormAddFriend: !state.showFormAddFriend})
 
-  const handleChangeGetFriendName = (e) => {
-    setFriendName(e.target.value)
-  }
+  const handleChangeGetFriendName = (e) => dispatch({type: "set_friendName", friendName: e.target.value})
 
-  const handleChangeGetFriendPhoto = (e) => {
-    setFriendPhoto(e.target.value)
-  }
+  const handleChangeGetFriendPhoto = (e) =>dispatch({type: "set_friendPhoto", friendPhoto: e.target.value})
 
   const handleClickAddFriend = () => {
-    setFriends((prev) => (
-      [...prev, {id: crypto.randomUUID(),
-        name: friendName,
-        photo: friendPhoto,
-        balance: 0,
-        selected: false,}]
-        ))
-    setFriendName("")
-    setFriendPhoto("")
-    setShowFormAddFriend(false)
+    dispatch({type: "set_friends", friends: [...state.friends, {id: crypto.randomUUID(), name: state.friendName, photo: state.friendPhoto, balance: 0, selected: false,}]})
+    dispatch({type: "set_friendName", friendName: ""})
+    dispatch({type: "set_friendPhoto", friendPhoto: ""})
+    dispatch({type: "set_showFormAddFriend", showFormAddFriend: false})
   }
 
   return (
@@ -121,9 +96,9 @@ const App = () => {
       <main className="app">
         <aside className="sidebar">
           <ul>
-            {friends?.map((friend) => {
+            {state.friends?.map((friend) => {
               const { message, color } = getMessageInfo(friend.balance);
-              const isSelectedFriend = friend.id === selectedFriend?.id;
+              const isSelectedFriend = friend.id === state.selectedFriend?.id;
 
               return (
                 <li key={friend.id}>
@@ -142,56 +117,35 @@ const App = () => {
               );
             })}
           </ul>
-          {showFormAddFriend && (
+          {state.showFormAddFriend && (
             <form className="form-add-friend" onSubmit={handleClickAddFriend}>
               <label>
-              🧍‍♂️ Nome
-              <input
-                type="text"
-                value={friendName}
-                onChange={handleChangeGetFriendName}
-              />
+              🧍‍♂️ Nome <input type="text" value={state.friendName} onChange={handleChangeGetFriendName}/>
             </label>
             <label>
-              📷 Foto
-              <input
-                type="text"
-                value={friendPhoto}
-                onChange={handleChangeGetFriendPhoto}
-              />
+              📷 Foto <input type="text" value={state.friendPhoto} onChange={handleChangeGetFriendPhoto}/>
             </label>
             <button className="button">Adicionar</button>
             </form>
           )}
 
-          <button className={`button ${showFormAddFriend ? "button-close" : ""}`} onClick={handleClickShowAddFriend}>{showFormAddFriend ? "Fechar" : "Adicionar Amigo(a)"}</button>          
+          <button className={`button ${state.showFormAddFriend ? "button-close" : ""}`} onClick={handleClickShowAddFriend}>{state.showFormAddFriend ? "Fechar" : "Adicionar Amigo(a)"}</button>          
         </aside>
 
-        {selectedFriend && (
+        {state.selectedFriend && (
           <form className="form-split-bill" onSubmit={handleClickCalculateBill}>
-            <h2>Rache a conta com {selectedFriend?.name}</h2>
+            <h2>Rache a conta com {state.selectedFriend?.name}</h2>
             <label>
-              💰 Valor total
-              <input
-                type="number"
-                value={totalBill}
-                onChange={handleChangeGetTotalBill}
-              />
+              💰 Valor total <input type="number" value={state.totalBill} onChange={handleChangeGetTotalBill}/>
             </label>
             <label>
-              💸 Seus gastos
-              <input
-                type="number"
-                value={costs}
-                onChange={handleChangeGetCosts}
-              />
+              💸 Seus gastos <input type="number" value={state.costs} onChange={handleChangeGetCosts}/>
             </label>
             <label>
-              🤑 Quem vai pagar
-              <select onChange={handleSelectWhoWillPay}>
+              🤑 Quem vai pagar <select onChange={handleSelectWhoWillPay}>
                 <option value="you">Você</option>
-                <option value={selectedFriend?.name}>
-                  {selectedFriend?.name}
+                <option value={state.selectedFriend?.name}>
+                  {state.selectedFriend?.name}
                 </option>
               </select>
             </label>
